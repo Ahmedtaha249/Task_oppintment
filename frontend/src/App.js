37d6ebc4cc93ec1d0e05e9cf432dbb456a7b0661
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-12-27 20:24:24 -0800

    created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,9 @@ import {Route,  Routes,Router} from "react-router-dom";
 import  RegisterBuyer from "./Components/Buyer/SignUp";
 import RegisterSeller from "./Components/Seller/SignUpSeller";
 import Login  from "./Components/Buyer/Login";
-import LoginSeller from "./Components/Seller/LoginSeller"
-import  AllSellers from "./Components/dashboardSeller/dashboardSeller"
+import LoginSeller from "./Components/Seller/LoginSeller";
+import  AllSellers from "./Components/dashboardSeller/dashboardSeller";
+import Search from "./Components/searchResult/searchResult"
 export const  infor=createContext()
 const App = () => {
   
@@ -22,7 +23,9 @@ const App = () => {
        <Route exact path="/loginSeller" element={<LoginSeller/>}/>
        <Route exact path="/signUpBuyer" element={<RegisterBuyer/>}/>
        <Route exact path="/signUpSeller" element={<RegisterSeller/>}/>
+       
        <Route exact path="/homeBuyer" element={<AllSellers/>}/>
+       <Route exact path="/searchResul/:id/" element={<Search/>}/> 
        </Routes>
        </infor.Provider>
       </div> 
--- frontend/src/Components/dashboardSeller/dashboardSeller.js
+++ frontend/src/Components/dashboardSeller/dashboardSeller.js
@@ -1,46 +1,112 @@
-import React, { useEffect, useState,useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
-import "./dash.css"
+import "./dash.css";
+import { useNavigate } from "react-router-dom";
 import { infor } from "../../App";
-const AllSellers=()=>{
-    const newUser = useContext(infor);
-    const [select, setSelect] = useState();
-
-  const getSellers=()=>{
-      axios.get("http://localhost:5000/signUpSeller",{headers: { Authorization: `Bearer ${newUser.token}` },
-    }).then((result) => {
-       
+const AllSellers = () => {
+  const history = useNavigate();
+  const newUser = useContext(infor);
+  const [select, setSelect] = useState();
+  const [check, setCheck] = useState();
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [phone, setPhone] = useState();
+  const getSellers = () => {
+    axios
+      .get("http://localhost:5000/signUpSeller", {
+        headers: { Authorization: `Bearer ${newUser.token}` },
+      })
+      .then((result) => {
         setSelect(result.data.result);
       })
       .catch((err) => {
         console.log(err);
       });
-  }
+  };
 
   useEffect(() => {
     getSellers();
   }, []);
 
+  const srch = () => {
+    axios
+      .get("http://localhost:5000/signUpSeller/search")
+      .then((result) => {
+        console.log(result.data.result)
+        setCheck(result.data.result);
+        console.log(result._id)
+      
+     
+      })
+      
+      .catch((err) => {
+        console.log(err);
+      });
+     
+  };
 
 
 
-    return(
-        <div className="allSeller">
-         {select&&select.map((elem,index)=>{
-             return(
-                 <div key={index} className="allSellerInner">
+  return (<>
+  
+ 
+ 
+     <div className="search">
+      
+    <input
+      type="text"
+      placeholder="firstName here"
+      onChange={(e) => {
+        setFirstName(e.target.value);
+      }}
+    />
 
-                   <img src="https://img.freepik.com/free-vector/appointment-booking-calendar-design_23-2148573174.jpg?size=338&ext=jpg" alt="appointment"/>
-                  <h2>firstName :{elem.firstName}</h2>
-                  <h2> lastName :{elem. lastName}</h2>
-                  <h2>email:{elem.email}</h2>
-                  
-                  <p>description:{elem. description}</p>
-                  <h2>  phone:{elem. phone}</h2>
-                 </div>
-             )
-         })}
-        </div>
-    )
-}
+    <input
+      type="text"
+      placeholder="lastName here"
+      onChange={(e) => {
+        setLastName(e.target.value);
+      }}
+    />
+
+    <input
+      type="tel"
+      placeholder="Phone here"
+      onChange={(e) => {
+        setPhone(e.target.value);
+      }}
+    /> 
+    <button
+      onClick={() => {
+        srch();
+      }}
+    >
+      Search
+    </button>
+  </div>
+  
+    <div className="allSeller"> 
+    
+
+      {select &&
+        select.map((elem, index) => {
+          return (
+            <div key={index} className="allSellerInner">
+              <img
+                src="https://img.freepik.com/free-vector/appointment-booking-calendar-design_23-2148573174.jpg?size=338&ext=jpg"
+                alt="appointment"
+              />
+              <h2>firstName :{elem.firstName}</h2>
+              <h2> lastName :{elem.lastName}</h2>
+              <h2>email:{elem.email}</h2>
+
+              <p>description:{elem.description}</p>
+              <h2> phone:{elem.phone}</h2>
+            </div>
+          );
+        })}
+    </div>
+    </>
+  );
+};
 export default AllSellers;
